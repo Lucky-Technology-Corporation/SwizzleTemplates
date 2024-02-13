837aadb379a8e63f00db500af9ecd3e46fb2a8a8
Adam Barr-Neuwirth <6325943+slugiscool99@users.noreply.github.com>
2024-02-13 10:35:39 -0800

    add frontend

--- postgres.ts
+++ postgres.ts
@@ -102,3 +102,110 @@ const pool = new pg.Pool({ connectionString: process.env.POSTGRES_CONNECTION_STR
 export default function getPostgresDB() {
   return pool
 }
+//_SWIZZLE_FILE_PATH_frontend/src/pages/SwizzleHomePage.tsx
+import api from '../Api'
+import { useEffect, useState } from 'react'
+import SignIn from '../components/SignIn'
+
+function Home() {
+  const [postgresTablesResult, setPostgresTablesResult] = useState([])
+  const [selectedTable, setSelectedTable] = useState(null)
+  const [tableData, setTableData] = useState(null)
+  const [headers, setHeaders] = useState(null)
+
+  useEffect(() => {
+    api
+      .get('/postgres/tables')
+      .then((result) => {
+        setPostgresTablesResult(result.data)
+      })
+      .catch((error) => {
+        console.error(error.message)
+      })
+  }, [])
+
+  useEffect(() => {
+    if (!selectedTable) return
+
+    api
+      .get(`/postgres/${selectedTable}`, {
+        params: {
+          size: 10,
+          page: 0,
+        },
+      })
+      .then((result) => {
+        setTableData(result.data)
+        if (result.data.length > 0) {
+          setHeaders(Object.keys(result.data[0]))
+        } else {
+          setHeaders(null)
+        }
+      })
+      .catch((error) => {
+        console.error(error.message)
+      })
+  }, [selectedTable])
+
+  return (
+    <div className="bg-gray-800 h-screen flex flex-col items-center text-gray-300">
+      <SignIn />
+      {/* Dropdown for selecting tables */}
+      {postgresTablesResult.length > 0 && (
+        <div className="mt-4 flex items-center gap-3 mx-4">
+          <label
+            htmlFor="table-select"
+            className="text-sm font-medium text-gray-300"
+          >
+            Select a table:
+          </label>
+          <div className="flex-grow">
+            <select
+              id="table-select"
+              className="bg-gray-700 border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              onChange={(event) => setSelectedTable(event.target.value)}
+            >
+              {postgresTablesResult.map((table, index) => (
+                <option key={index} value={table}>
+                  {table}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      )}
+      {tableData && headers && (
+        <table className="min-w-full leading-normal">
+          <thead>
+            <tr>
+              {headers.map((header, index) => (
+                <th
+                  key={index}
+                  className="px-5 py-3 border-b-2 border-gray-700 bg-gray-900 text-left text-xs font-semibold text-gray-400 uppercase tracking-wider"
+                >
+                  {header}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {tableData.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {headers.map((header, headerIndex) => (
+                  <td
+                    key={headerIndex}
+                    className="px-5 py-2 border-b border-gray-700 bg-gray-800 text-sm"
+                  >
+                    {row[header]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  )
+}
+
+export default Home
